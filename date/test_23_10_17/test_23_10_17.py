19d767a6b0a4ba2c48ae83c5ee8f89a9ba43aac5
longxixyz <longxi347@gmail.com>
2023-10-17 23:50:36 +0800

    代码练习

---
+++ date/test_23_10_17/test_23_10_17.py
@@ -0,0 +1,12 @@
+#读取文档内容
+""" from pathlib import Path
+
+get_txt=Path('txt_23_10_17(1).txt')
+read_txt=get_txt.read_text()
+print(read_txt) """
+
+from pathlib import Path
+
+get_txt=Path('txt_23_10_17(2).txt')
+read_txt=get_txt.read_text()
+print(read_txt)
